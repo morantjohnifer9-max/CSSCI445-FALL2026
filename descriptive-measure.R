7abff60d95023a88d6648b8d10325a9a0b04e210
morantjohnifer9-max <morantjohnifer9@gmail.com>
2026-08-18 16:15:38 -0400

    Add files via upload

---
+++ descriptive-measure.R
@@ -0,0 +1,16 @@
+#Name: Johnifer Date:08/18/2026, Purpose: To test
+function of descriptive statistics
+
+# Create a variable-called  variable1 and store 5 
+numbers from 1-5
+
+variable1<-c(1,2,3,4,5)
+
+# calculate mean of variable1
+
+mean(variable1)
+[1]3
+
+#Calcuate standard deviation
+sd(variable1)
+[1] 1.581139
